0f072cd1aad7c7640a9998fe22b141667cf7fb93
Lucas Duarte da Silva <lukazds2016@gmail.com>
2023-05-22 14:57:29 -0300

    feat: add pop up in menu header

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    const closeImgMenu = document.querySelector(".header_menu_img_close")
+    closeImgMenu.classList.toggle("header_menu_img_open")
+
+    const closeListMenu = document.querySelector(".menu_list_close")
+    closeListMenu.classList.toggle("menu_list_open")
+}
